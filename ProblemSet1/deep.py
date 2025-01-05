a1983d7b6b55625496ba4428813c0ac6678d750b
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2025-01-05 21:26:51 +0500

    Create deep.py

---
+++ ProblemSet1/deep.py
@@ -0,0 +1,5 @@
+answer=input("what is  the answer to the Great Question of Life, the Universe and Everything:").strip()
+if answer.strip()=="42" or answer.lower()=="forty-two" or answer.lower()=="forty two":
+    print("Yes")
+else:
+    print("No")
