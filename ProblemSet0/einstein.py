4dca17f13cf5e2cee5c5a8c0ec00d28c42351c98
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2025-01-05 21:22:53 +0500

    Create einstein.py

---
+++ ProblemSet0/einstein.py
@@ -0,0 +1,6 @@
+mass = int(input("enter the mass the in kg")) #take input from user and store into variable
+
+energy = mass*(300000000)**2 #  the input with 300000000
+
+# then print the output
+print(str(energy)
