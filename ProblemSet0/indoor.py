66be445182ddee90a46310a72f91ce49dd38b3c4
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2025-01-05 21:23:59 +0500

    Create indoor.py

---
+++ ProblemSet0/indoor.py
@@ -0,0 +1,6 @@
+#input from user and store into a varible
+upper_case =input("please enter in upper case")
+#use lower() function to make upper case into lower case
+lower_case = upper_case.lower()
+#print() the lower case as output
+print(lower_case)
