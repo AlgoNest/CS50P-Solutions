52a63c01eb4f8f753dddc7a26b26ea57fa9f26af
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2025-01-27 20:28:51 +0500

    Create um.py

---
+++ Problemset7/um.py
@@ -0,0 +1,17 @@
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    matches = re.findall(r"\b\W*um\W*\b", s, re.IGNORECASE)
+    return len(matches)
+...
+
+
+if __name__ == "__main__":
+    main()
+
